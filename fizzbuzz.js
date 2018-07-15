5e72302a2c79f562cac8d5ff62ee08d7e3b0a6aa
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2018-07-15 11:51:10 +0300

    Added comments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,11 @@
 "use strict";
 // seperate the if statement from the loop for better functionality and maintenance (granularity).
+
+// Assert the right outcome for each input
 function assert(i){
 		// if statement: the order matters and the AND condition should be first otherwise 
-		// I'll get only 3 OR 5 from the other 2 conditions as it'll never got to the AND condition. 
-		// Also, commas and spaces are added to make it easier to read.
+		// I'll get only 3 OR 5 from the other 2 conditions as it'll never get to the AND condition. 
+		// Also, dots and spaces are added to make it easier to read.
 		if ((i % 3 === 0) && (i % 5 === 0)) {
 			// if divisible by 3 AND 5, return "fizzbuzz".
 			return "fizzbuzz. ";
@@ -19,14 +21,20 @@ function assert(i){
 		}
 };
 
+// Use the "assert" function and loop through any given number of items
 function fizzbuzz(maxNumber){
+	// Create a variable for the result that'll be shown to the user
 	var show = "";		
+	// loop through the items
 	for (var i = 1; i <= maxNumber; i++) {
+		//Add the values to the result variable
 		show += assert(i);
 	};
+	// Return the end result to be shown to the user
 	return show;
 };
 
+// Start the page with the first answer - a 100 items - shown on the "outcome" paragraph
 let outcome = document.querySelector("#outcome");
 outcome.textContent = fizzbuzz(100);
 
@@ -34,6 +42,7 @@ outcome.textContent = fizzbuzz(100);
 // Allow the users to enter a number of their own and get the fizzbuzz result for it (not only for a 100 numbers)
 const num = document.querySelector("#num");
 const input = document.querySelector("#input");
+
 input.addEventListener("keypress", (event) => {
 	if (event.keyCode === 13) {
 		outcome.textContent = fizzbuzz(input.value);
@@ -44,6 +53,8 @@ input.addEventListener("keypress", (event) => {
 	}
 });
 
+
+// Allow the users to enter a single number and get it's specific outcome
 const singleAnswer = document.querySelector("#singleAnswer");
 const singleResult = document.querySelector("#singleResult");
 singleResult.addEventListener("keypress", (event) => {
